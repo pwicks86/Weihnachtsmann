e77bcd740450160f78616ac61952b1061c9ef817
Paul Wicks <pwicks86@gmail.com>
2017-12-09 15:16:27 -0800

    BWFade done

--- xmas_final.py
+++ xmas_final.py
@@ -15,6 +15,10 @@ touch2 = TouchIn(board.A2)
 numpix = 60
 strip = neopixel.NeoPixel(board.D1, numpix, brightness=1, auto_write=False)
 
+blue = (0,0,255)
+white = (255,255,255)
+black = (0,0,0)
+
 def set_all(c):
     for i in range(numpix):
         strip[i] = c
@@ -24,6 +28,9 @@ def clear():
 
 def rand_color():
     return tuple(sorted((0, 0xFF, random.randrange(0,255)), key = lambda x: random.random()))
+
+def rand_led():
+    return random.randrange(0, numpix)
 
 # Xmas colors
 class RedGreenMarch():
@@ -56,7 +63,7 @@ class ColorFlash():
             self.color = rand_color()
             self.cycle_num = 0
         mult = (self.cycles_to_black - self.cycle_num) / self.cycles_to_black
-        set_all(tuple([int( mult * x) for x in self.color]))
+        set_all(tuple([int(mult * x) for x in self.color]))
         strip.write()
         self.cycle_num += 1
 
@@ -111,17 +118,38 @@ class Sparkle():
         self.clear_list = []
     def run(self):
         for i in self.clear_list:
-            strip[i] = (0,0,0)
+            strip[i] = black
         self.clear_list.clear()
         for i in range(numpix/3):
             idx = random.randrange(0,numpix - 1)
-            strip[idx] = (255,255,255)
+            strip[idx] = white
             self.clear_list.append(idx)
         strip.write()
 
-modes = [Sparkle, FunFill, Falling, RandomJunk, ColorFlash, ]
+class BWFade():
+    def __init__(self):
+        clear()
+        self.fades = []
+    def run(self):
+        while len(self.fades) < 20:
+            l = rand_led()
+            skip = False
+            for f in self.fades:
+                if l == f[0]:
+                    skip = True
+            if skip:
+                continue
+            self.fades.append([l, random.choice([white, blue]),random.randrange(5, 20)])
+        for f in self.fades:
+            strip[f[0]] = f[1]
+            f[1] = (max(f[1][0] - f[2], 0), max(f[1][1] - f[2], 0), max(f[1][2] - f[2], 0))
+        strip.write()
+        self.fades = list(filter(lambda f: f[1][2] > 0, self.fades))
+
+
+modes = [BWFade ,Sparkle, FunFill, Falling, RandomJunk, ColorFlash, ]
 num_modes = len(modes)
-mode_index = 0
+mode_index = 1
 active_mode = modes[mode_index]()
 
 last_button = button.value
