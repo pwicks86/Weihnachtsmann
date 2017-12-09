619be4b69237ec7fcfe3fbdc56e8383822bd0433
Paul Wicks <pwicks86@gmail.com>
2017-12-09 15:28:36 -0800

    Remove dead mode

--- xmas_final.py
+++ xmas_final.py
@@ -34,26 +34,6 @@ def rand_color():
 def rand_led():
     return random.randrange(0, numpix)
 
-# Xmas colors
-class RedGreenMarch():
-    def wheel(pos):
-        # Input a value 0 to 255 to get a color value.
-        # The colours are a transition r - g - b - back to r.
-        if (pos < 0):
-            return [0, 0, 0]
-        if (pos > 255):
-            return [0, 0, 0]
-        if (pos < 85):
-            return [int(pos * 3), int(255 - (pos*3)), 0]
-        elif (pos < 170):
-            pos -= 85
-            return [int(255 - pos*3), 0, int(pos*3)]
-        else:
-            pos -= 170
-            return [0, int(pos*3), int(255 - pos*3)]
-    def run(self, s):
-        pass
-
 # Flash and fade
 class ColorFlash():
     def __init__(self):
